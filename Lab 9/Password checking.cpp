f662e5b2ad0a6bad2ad2ced63e94723ec7f71694
Wajeeha Batool <wb140507@gmail.com>
2026-01-06 11:44:00 +0500

    do while tasks

---
+++ Lab 9/Password checking.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string password1;
+	string password="wajeehabatool";
+	
+
+	do
+	{
+		cout<<"Enter password: ";
+		cin>>password1;
+	}
+	while(password1!=password);
+	
+	return 0;
+}
